8f0ff033758b24c4fabbd1b98c9fe44081787e20
Fabio Ribeiro <faabiosr@gmail.com>
2024-05-25 15:59:32 +0200

    pecl

--- scripts/pecl-install.sh
+++ scripts/pecl-install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -e
-__CURRENT__=$(cd "$(dirname "$0")";pwd)
-__DIR__=$(cd "$(dirname "${__CURRENT__}")";pwd)
 
-pecl package
-pecl install --configureoptions 'with-libzookeeper-dir="/opt/libzookeeper"' zookeeper*.tgz
+phpize
+./configure $@
+make
+make install
+
 echo "extension=zookeeper.so" >> $(php -i | grep /.+/php.ini -oE)
 php --ri zookeeper
